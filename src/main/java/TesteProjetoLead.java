ad2d74306f3aa2442b15db4a3b6ec6d1e65bd73c
Katarina Galdino <katarinagaldino24@gmail.com>
2019-02-04 17:44:00 -0300

    #Update

--- src/main/java/TesteProjetoLead.java
+++ src/main/java/TesteProjetoLead.java
@@ -1,3 +1,5 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
@@ -5,50 +7,25 @@ import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TesteProjetoLead extends Manager{
+public class TesteProjetoLead{
 	
-	@Test
-	public void testeLogin() {
-		WebDriver driver = new ChromeDriver();
+	private WebDriver driver;
+	
+	@Before
+	public void BeforeMethod() {
+		System.setProperty("webdriver.chrome.driver", "/Users/Katarina/Downloads/chromedriver_win32/chromedriver.exe");	
+		driver = new ChromeDriver();
 		driver.manage().window().setPosition(new Point(0, 0));
-		driver.manage().window().setSize(new Dimension(1200, 700));
 		driver.get("https://teste.projetolead.com.br/ead2pcd/app/login?ctrl=1");
-		
-		driver.findElement(By.id("usuario")).sendKeys("alunosurdo");
-		driver.findElement(By.id("senha")).sendKeys("abcd1234");
-		driver.findElement(By.name("_spring_security_remember_me")).click();
-		driver.findElement(By.tagName("button")).click();
-		
-		driver.findElement(By.id("imgUsuario")).click();
-		driver.findElement(By.id("logoff")).click();
-		
-		driver.quit();
-		
 	}
 	
-	@Test
-	public void testeRecuperaSenha() {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().setPosition(new Point(0, 0));
-		driver.manage().window().setSize(new Dimension(1200, 700));
-		driver.get("https://teste.projetolead.com.br/ead2pcd/app/login?ctrl=1");
-		
-		driver.findElement(By.linkText("Esqueci minha senha.")).click();
-		driver.findElement(By.id("formRecupera:usuario")).sendKeys("email.errado@gmail.com");
-		driver.findElement(By.id("formRecupera:enviar")).click();
-		
-		//System.out.println(driver.findElement(By.xpath("//div/div/div/ul/li")).getText());
-		//driver.findElement(By.tagName("div")).getText().contains("Erro no pedido de mudança de senha");
-		//driver.findElement(By.tagName("h4")).getText().contains("Pedido realizado com sucesso");
-		//driver.findElement(By.tagName("div")).getText().contains("Foi enviado para você um e-mail contendo informações para recuperação da senha.");
-		
-		driver.quit();		
+	@After
+	public void AfterMethod() {
+		driver.quit();
 	}
 	
 	@Test
 	public void testeLogout1() {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().setPosition(new Point(0, 0));
 		driver.manage().window().setSize(new Dimension(1200, 700));
 		driver.get("https://teste.projetolead.com.br/ead2pcd/app/login?ctrl=1");
 		
@@ -58,15 +35,12 @@ public class TesteProjetoLead extends Manager{
 		driver.findElement(By.tagName("button")).click();
 		
 		driver.findElement(By.id("imgUsuario")).click();
-		driver.findElement(By.id("logoff")).click();
-		
-		driver.quit();
-		
+		driver.findElement(By.id("logoff")).click();	
 	}
+	
+	
 	@Test
 	public void testeLogout2() {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().setPosition(new Point(0, 0));
 		driver.manage().window().setSize(new Dimension(700, 700));
 		driver.get("https://teste.projetolead.com.br/ead2pcd/app/login?ctrl=1");
 		
@@ -76,32 +50,35 @@ public class TesteProjetoLead extends Manager{
 		driver.findElement(By.tagName("button")).click();
 		
 		driver.findElement(By.id("imgUsuario")).click();
-		driver.findElement(By.id("logoff")).click();
-		
-		driver.quit();
-		
+		driver.findElement(By.id("logoff")).click();	
 	}
 	
 	
 	@Test
-	public void testeLogout3() {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().setPosition(new Point(0, 0));
+	public void testeRecuperaSenha_EmailValido() {
 		driver.manage().window().setSize(new Dimension(1200, 700));
-		driver.get("https://teste.projetolead.com.br/ead2pcd-spa/");
+		driver.get("https://teste.projetolead.com.br/ead2pcd/app/login?ctrl=1");
 		
-		driver.findElement(By.id("login")).sendKeys("alunosurdo");
-		driver.findElement(By.id("inputPassword")).sendKeys("abcd1234");
-		driver.findElement(By.name("remember")).click();
-		driver.findElement(By.tagName("button")).click();
+		driver.findElement(By.linkText("Esqueci minha senha.")).click();
+		driver.findElement(By.id("formRecupera:usuario")).sendKeys("katarinagaldin@gmail.com");
+		driver.findElement(By.id("formRecupera:enviar")).click();
 		
-		driver.findElement(By.id("avatar")).click();
-		driver.findElement(By.id("logout")).click();
-		
-		driver.quit();
-		
+		driver.findElement(By.tagName("h4")).getText().contains("Pedido realizado com sucesso");
+		driver.findElement(By.tagName("div")).getText().contains("Foi enviado para você um e-mail contendo informações para recuperação da senha.");		
 	}
+
 	
+	@Test
+	public void testeRecuperaSenha_EmailInvalido() {
+		driver.manage().window().setSize(new Dimension(1200, 700));
+		driver.get("https://teste.projetolead.com.br/ead2pcd/app/login?ctrl=1");
+		
+		driver.findElement(By.linkText("Esqueci minha senha.")).click();
+		driver.findElement(By.id("formRecupera:usuario")).sendKeys("email.errado@gmail.com");
+		driver.findElement(By.id("formRecupera:enviar")).click();
+		
+		driver.findElement(By.tagName("div")).getText().contains("Erro no pedido de mudança de senha");		
+	}
 	
 	
 	
